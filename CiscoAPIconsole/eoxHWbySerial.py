b9b2510961bf37fd26a47fee66151d63f4a3db5e
msiegy <msiegy88@gmail.com>
2016-12-05 14:07:33 -0500

    Added logic to include Errors for products when they are not found.

--- CiscoAPIconsole/eoxHWbySerial.py
+++ CiscoAPIconsole/eoxHWbySerial.py
@@ -2,7 +2,9 @@
 [csvInputPath] and uses the EOX V5 API within Cisco API Console to retrieve  and the write the associated EoX
 information to a CSV.
 
-    PsuedoCode: For Each SW Release In 'csvInputPath' Run get_eoxbySerial and append EoS, LDoS, etc tuples in 'csvWritePath'
+
+
+    PsuedoCode: For Each Serial In 'csvInputPath' Run get_eoxbySerial and append EoS, LDoS, etc tuples in 'csvWritePath'
 """
 
 import requests
@@ -16,7 +18,11 @@ csvInputPath = 'C:/Users/matsiege/PycharmProjects/Templates/CiscoAPIconsole/hwin
 csvWritePath = 'C:/Users/matsiege/PycharmProjects/Templates/CiscoAPIconsole/eoxHWresults.csv'
 
 def get_eoxbySerial(hwPID):
-    """returns HW EoX info when providing HW Serial"""
+    """returns HW EoX info when providing HW Serial
+    The resulting data structure is a Dictionary, that contains elements of Dictionaries and Lists. e.g: EOXRecord is a
+    list and the first element happens to be an unnamed dictionary, so it is identified with [0]. Within this unnamed
+    dictionary there are Key/Value pairs representing PIDs and EoX dates.
+    """
 
     token = get_token()
 
@@ -31,19 +37,19 @@ def get_eoxbySerial(hwPID):
 
     return response.text
 
-def writecsv(PID, EoS, EoM, LDoS, URL, MigrationPID, MigrationURL, MigrationStrat):
+def writecsv(PID, EoS, EoM, LDoS, URL, MigrationPID, MigrationURL, MigrationStrat, ErrorDescription):
     """Appends HWPID, EoS, EoM, LDoS and URL Bulletins to CSV file defined at top."""
 
     """Set File existence flag, If results file does not yet exist we will write Field Headers to the CSV"""
     file_exists = os.path.isfile(csvWritePath)
 
     with open(csvWritePath, 'ab') as csvfile:
-        fieldnames = ['PID', 'EoS', 'EoM', 'LDoS', 'EoX URL', 'MigrationPID', 'MigrationURL', 'MigrationStrat']
+        fieldnames = ['PID', 'EoS', 'EoM', 'LDoS', 'EoX URL', 'MigrationPID', 'MigrationURL', 'MigrationStrat', 'Error Description']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         if not file_exists:
             writer.writeheader()
 
-        writer.writerow({'PID': PID, 'EoS': EoS, 'EoM': EoM, 'LDoS': LDoS, 'EoX URL': URL, 'MigrationPID': MigrationPID, 'MigrationURL': MigrationURL, 'MigrationStrat': MigrationStrat})
+        writer.writerow({'PID': PID, 'EoS': EoS, 'EoM': EoM, 'LDoS': LDoS, 'EoX URL': URL, 'MigrationPID': MigrationPID, 'MigrationURL': MigrationURL, 'MigrationStrat': MigrationStrat, 'Error Description': ErrorDescription})
 
 def parsecsv():
     """Reads CSV of Serials and Appends results of get_eoxbySerial() to CSV file csvWritePath defined at top
@@ -66,6 +72,12 @@ def parsecsv():
             MigrationPID = data['EOXRecord'][0]["EOXMigrationDetails"]["MigrationProductId"]
             MigrationURL = data['EOXRecord'][0]["EOXMigrationDetails"]["MigrationProductInfoURL"]
             MigrationStrat = data['EOXRecord'][0]["EOXMigrationDetails"]["MigrationStrategy"]
+            ErrorDescription = ""
+            if 'EOXError' in data['EOXRecord'][0]:
+                ErrorDescription = data['EOXRecord'][0]['EOXError']['ErrorDescription']
+                #print type(data['EOXRecord'][0])
+            else:
+                ErrorDescription = "No Error(s) found"
 
             """
             print PID + "::"
@@ -76,10 +88,13 @@ def parsecsv():
             print "Migration PID: " + MigrationPID
             print "Migration URL: " + MigrationURL
             print "Migration Strategy: " + MigrationStrat
+            print "Error Field: " + ErrorDescription
             print "-----"
             """
+            #print hwSerial
+            #print ErrorDescription
 
-            writecsv(PID, EoS, EoM, LDoS, URL, MigrationPID, MigrationURL, MigrationStrat)
+            writecsv(PID, EoS, EoM, LDoS, URL, MigrationPID, MigrationURL, MigrationStrat, ErrorDescription)
 
 
 
